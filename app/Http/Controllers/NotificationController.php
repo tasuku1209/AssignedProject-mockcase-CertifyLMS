c2cab3272947cea710d469a8d7b72174892dd40b
tasuku <gaojintasuku@gmail.com>
2026-08-25 00:28:16 +0900

    feat/S-B-04-アクション、コントローラーの実装

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\UseCases\Notification\IndexAction;
+use App\UseCases\Notification\MarkAllAsReadAction;
+use App\UseCases\Notification\MarkAsReadAction;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\View\View;
+
+class NotificationController extends Controller
+{
+    public function index(Request $request, IndexAction $action): View
+    {
+        $this->authorize('view', $request->user());
+
+        $result = $action(
+            $request->user(),
+            $request->query('tab', 'all'),
+        );
+
+        return view('notifications.index', $result);
+    }
+
+    public function markAsRead(
+        DatabaseNotification $notification,
+        MarkAsReadAction $action,
+    ): RedirectResponse {
+        $this->authorize('markAsRead', $notification);
+
+        $action($notification);
+
+        return redirect()->back();
+    }
+
+    public function markAllAsRead(
+        Request $request,
+        MarkAllAsReadAction $action,
+    ): RedirectResponse {
+        $this->authorize('markAllAsRead', $request->user());
+
+        $action($request->user());
+
+        return redirect()->back();
+    }
+}
