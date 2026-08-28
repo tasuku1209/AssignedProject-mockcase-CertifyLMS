b4a18f46b8577eac09b5249e322909bfed1b6444
tasuku <gaojintasuku@gmail.com>
2026-08-28 14:18:18 +0900

    feat/S-B-07-フォームリクエスト、アクション、コントローラー@store,@edit,@updateの実装

---
+++ app/Http/Controllers/EnrollmentNoteController.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EnrollmentNote\StoreRequest;
+use App\Http\Requests\EnrollmentNote\UpdateRequest;
+use App\Models\Enrollment;
+use App\Models\EnrollmentNote;
+use App\UseCases\EnrollmentNote\StoreAction;
+use App\UseCases\EnrollmentNote\UpdateAction;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+
+class EnrollmentNoteController extends Controller
+{
+    public function store(
+        StoreRequest $request,
+        Enrollment $enrollment,
+        StoreAction $action,
+    ): RedirectResponse {
+        $action(
+            $request->user(),
+            $enrollment,
+            $request->validated(),
+        );
+
+        return redirect()
+            ->route('enrollments.show', $enrollment)
+            ->with('success', 'メモを追加しました。');
+    }
+
+    public function edit(EnrollmentNote $note): View
+    {
+        $this->authorize('update', $note);
+
+        return view('enrollment-note.edit', [
+            'note' => $note,
+        ]);
+    }
+
+    public function update(
+        EnrollmentNote $note,
+        UpdateRequest $request,
+        UpdateAction $action,
+    ): RedirectResponse {
+        $action(
+            $note,
+            $request->validated(),
+        );
+
+        return redirect()
+            ->route('enrollments.show', $note->enrollment_id)
+            ->with('success', 'メモを更新しました。');
+    }
+}
