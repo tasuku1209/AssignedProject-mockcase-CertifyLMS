285831b3b1a90f5e096b622e3a5f0fbeaed0db12
tasuku <gaojintasuku@gmail.com>
2026-09-13 15:03:49 +0900

    feat/S-A-修正、GoogleCredentialControllerの配置を移動

--- app/Http/Controllers/GoogleCredentialController.php
+++ app/Http/Controllers/GoogleCredentialController.php
@@ -2,10 +2,9 @@
 
 declare(strict_types=1);
 
-namespace App\Http\Controllers\Settings;
+namespace App\Http\Controllers;
 
 use App\Exceptions\GoogleCalendar\InvalidOAuthStateException;
-use App\Http\Controllers\Controller;
 use App\Models\GoogleCredential;
 use App\Services\GoogleCalendarService;
 use App\UseCases\GoogleCredential\DestroyAction;
